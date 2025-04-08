5d6b3b88d6e1784f2929cc50c50ed24521332edb
aryanbeni <aryanbeni06@gmail.com>
2025-04-08 11:48:31 +0530

    WIP map screen

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -10,6 +10,38 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+              right: 0,
+              child: Container(
+            width: double.infinity,
+            height: 255,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              color: Colors.yellow
+            ),
+                child: Column(children: [
+                  Row(
+                    children: [
+                      Image.asset('assets/images/User.png',height: 40,width: 88,),
+                      Column(crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                        Text('Indoor Run',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                        Text('20km',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: Colors.black54),),
+                      ],)
+                    ],
+                  )
+                ],),
+          ))
+        ],
+      ),
+    );
   }
 }
