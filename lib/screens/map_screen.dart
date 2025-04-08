ec72da494b6cc085621460a357e4bbbd1413a5ea
aryanbeni <aryanbeni06@gmail.com>
2025-04-08 14:09:51 +0530

    WIP map screen

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -19,26 +19,77 @@ class _MapScreenState extends State<MapScreen> {
               right: 0,
               child: Container(
             width: double.infinity,
-            height: 255,
+            // height: 255,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(30),
                 topRight: Radius.circular(30),
               ),
-              color: Colors.yellow
+              color: Colors.white
             ),
-                child: Column(children: [
+                child: Padding(padding: EdgeInsets.symmetric(vertical: 20,horizontal: 24),child: Column(children: [
                   Row(
                     children: [
                       Image.asset('assets/images/User.png',height: 40,width: 88,),
+                      SizedBox(width: 25,),
                       Column(crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                        Text('Indoor Run',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-                        Text('20km',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: Colors.black54),),
-                      ],)
+                          Text('Indoor Run',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                          Text('20km',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: Colors.grey),),
+                        ],),
+                      Spacer(),
+                      Image.asset('assets/images/grid.png',height: 24,width: 24,),
                     ],
-                  )
-                ],),
+                  ),
+                  SizedBox(height: 15,),
+                  Divider(
+                    color: Colors.black26,
+                    thickness: 1,
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Column(children: [SizedBox(height: 15,),
+                        Image.asset('assets/images/run.png',height: 60,width: 60,),
+                      ],),
+                      SizedBox(width: 15,),
+                      Text.rich(TextSpan(children:[
+                      TextSpan(text: 'Elapsed time\n',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: Colors.grey),),TextSpan(text: '00 : 30 : 24',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),])),
+                      Spacer(),
+                      Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: 'Distance\n',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '12.5km',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '/20km',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 15,),
+                    ],
+                  ),
+                ],),),
+
           ))
         ],
       ),
